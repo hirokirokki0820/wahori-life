02974f534d1d7fdc776ccd694e62e5d635e61efb
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-27 19:42:08 +0900

    Add: Semantic UI のインストールとセットアップ

--- Gemfile
+++ Gemfile
@@ -46,7 +46,11 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
+
+# Semantic UI
+gem 'semantic-ui-sass'
+gem 'jquery-rails'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// = require semantic-ui
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+
+ /* Semantic UI 用の jquery関数 */
+$(document).on("turbo:load", function() {
+
+  /* ドロップダウン */
+  $('.ui.dropdown').dropdown();
+
+  /* アコーディオン */
+  $('.ui.accordion').accordion();
+
+});
